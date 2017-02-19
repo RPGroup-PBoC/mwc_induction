165418f6f2a1ca49691270e805a397755f21ed5f
Griffin Chure <gchure@caltech.edu>
2017-02-19 12:26:28 -0800

    Added notebooks

--- _site/data/microscopy/20161020/correct_metadata_naming.sh
+++
@@ -1,9 +0,0 @@
-# Some files were accidentally incorrectly named  and corrected in Windows,
-# but this does not change the metadata embedded within the image. This 
-# script corrects the improperly named files.
-unset LANG
-for dir in *auto_1000uMIPTG*/; do
-	LC_ALL=C sed -i '' 's/RBS1027/auto/g' $dir/*
-done
-
-
